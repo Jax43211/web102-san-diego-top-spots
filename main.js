d34ac4f869cf8f1afeea58aace939399ecc16d09
Jax43211 <thomasojacksoniii@gmail.com>
2025-10-17 14:57:28 -0600

    top spots code

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
-    // write your code here
+    $.getJSON('data.json', function(data){
+        $.each(data, function(index, spot){
+        const mapLink = `https://www.google.com/maps?q=${spot.location[0]},${spot.location[1]}`;
+        const row = `
+            <tr>
+             <td>${spot.name}</td>
+             <td>${spot.description}</td>
+             <td><a href="${mapLink}" target="_blank">Open in Google Maps</a></td>
+            </tr>
+        `;
+
+        $('#table-body').append(row)
+    });
+}).fail(function() {
+    console.error('Failed to load JSON data');
+});
 
 });
